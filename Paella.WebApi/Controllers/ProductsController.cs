a61fb88680bce115a51a3ef806f5d4bb55d4f842
Evangelos Barmpas <evangelos.barmpas@betssongroup.com>
2019-11-04 20:09:02 +0100

    food ready

---
+++ Paella.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Paella.Application.UseCases.GetAll;
+
+namespace Paella.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IGetAllUseCase _getAllUseCase;
+
+        public ProductsController(IGetAllUseCase getAllUseCase)
+        {
+            _getAllUseCase = getAllUseCase;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var result = _getAllUseCase.Execute();
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProductById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
