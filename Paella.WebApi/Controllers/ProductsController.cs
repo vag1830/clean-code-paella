d743e45615fde331fbd677d53fcc426201e369df
Evangelos Barmpas <evangelos.barmpas@betssongroup.com>
2019-11-05 22:04:14 +0100

    the tortilla

--- Paella.WebApi/Controllers/ProductsController.cs
+++ Paella.WebApi/Controllers/ProductsController.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using Paella.Application.UseCases.GetAll;
+using Paella.Application.UseCases.GetById;
 
 namespace Paella.WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly IGetAllUseCase _getAllUseCase;
+        private readonly IGetByIdUseCase _getByIdUseCase;
 
-        public ProductsController(IGetAllUseCase getAllUseCase)
+        public ProductsController(
+            IGetAllUseCase getAllUseCase,
+            IGetByIdUseCase getByIdUseCase)
         {
             _getAllUseCase = getAllUseCase;
+            _getByIdUseCase = getByIdUseCase;
         }
 
         [HttpGet]
@@ -27,7 +32,14 @@ namespace Paella.WebApi.Controllers
         [Route("{id}")]
         public IActionResult GetProductById(Guid id)
         {
-            return Ok();
+            var result = _getByIdUseCase.Execute(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
         }
     }
 }
