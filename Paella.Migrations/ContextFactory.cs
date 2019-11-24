587c111ebf93f3654adc242722f3955a20b87497
Evangelos Barmpas <vaggelis_b@hotmail.com>
2019-11-24 13:39:09 +0100

    added initial migration db creation

---
+++ Paella.Migrations/ContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Paella.Infrastructure;
+
+namespace Paella.Migrations
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<PaellaDbContext>
+    {
+        public PaellaDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", true, true)
+              .Build();
+
+            var connectionString = config.GetConnectionString("SQLServer");
+
+            var optionsBuilder = new DbContextOptionsBuilder<PaellaDbContext>();
+
+            optionsBuilder.UseSqlServer(connectionString, builder => builder.MigrationsAssembly("Paella.Migrations"));
+
+            return new PaellaDbContext(optionsBuilder.Options);
+        }
+    }
+}
